1bc22a67a6022272c287ade56f575d8fd5acf3b8
ThinhPhan <thinhpd@mor.com.vn>
2021-10-12 09:09:29 +0700

    admin

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * comment
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class TestController
+ *
+ * @package App\Http\Controllers
+ */
+class TestController extends Controller
+{
+    /**
+     * comment
+     */
+    public function demo()
+    {
+        $a = 1;
+        if ($a == 0) {
+            $b = 2;
+            $c = 3;
+        }
+    }
+}
