7011919e30ab86042a7a7c366f20971be4b4a487
nguyenmanhchien <chien.nguyenmanh@mor.com.vn>
2021-10-13 09:04:00 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/test', 'Api\DemoController@getDemo')->name('getDemo');
+Route::get('/test/test', 'Api\TestController@index')->name('base');
+Route::get('/order', 'Api\OrderController@index')->name('order');
+Route::get('/product', 'Api\ProductController@index')->name('product');
+Route::post('/product/store', 'Api\ProductController@add')->name('product_store');
+Route::put('/product/update/{id}', 'Api\ProductController@edit')->name('product_edit');
+Route::delete('/product/delete/{id}', 'Api\ProductController@delete')->name('product_delete');
